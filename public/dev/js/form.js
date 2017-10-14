0c9459a46c9f1d29a4b5daec6fbf473cfa36e0be
Kaluchii <mr.kaluchii@mail.ru>
2017-10-14 12:51:08 +0600

    Horizontal scroll fix

--- public/dev/js/form.js
+++ public/dev/js/form.js
@@ -1,1 +1,203 @@
-$(document).ready(function(){$('form').submit(function(e){e.stopPropagation();e.preventDefault();});function isEmail(mail){var regex=/\S+@\S+/igm;return regex.test(mail);}function clearFields(selector){$(selector).each(function(){if($(this).attr('name')!='form'){$(this).val('');$(this).parent().removeClass('valid');}});}function fieldValid(input){if(input.val()!=''){if(input.attr('type')=='email'){return isEmail(input.val());}else{return true;}}else{return false;}}function fieldCheck(input){var parent=input.parent();if(fieldValid(input)){if(parent.hasClass('error')){parent.removeClass('error')}parent.addClass('valid');return true;}else{if(parent.hasClass('valid')){parent.removeClass('valid')}parent.addClass('error');return false;}}$('.form-input').on('change',function(){return fieldCheck($(this));});$('.form-input').on('focusout',function(){$(this).parent().removeClass('filling');});$('.form-input').on('input',function(){$(this).parent().addClass('filling');});function fieldsCheck(selector){var checked=true;var focus=true;$(selector).each(function(){checked=fieldCheck($(this))&&checked;if(focus&&!checked){$(this).focus();focus=false;}});return checked;}function addFields(selector,object){$(selector).each(function(){var $this=$(this);object[$this.attr('name')]=$this.val();});}function ajaxDataSend(type,url,data){return $.ajax({type:type,url:url,dataType:'json',data:data,headers:{'X-CSRF-TOKEN':$('meta[name="csrf-token"]').attr('content')}});}var unical,sendButton,selector;var active=true;function finalValidation(){if(active){sendButton=$(this);unical=sendButton.closest('.form-id').attr('id');selector='#'+unical+' .form-input';var validForm=fieldsCheck(selector);if(validForm){active=false;sendButton.addClass('load');sendForm();}}}$('.send-form').on('click',finalValidation);function sendForm(){var dataobj={};addFields(selector,dataobj);var response=ajaxDataSend('POST','/feedback/mail',dataobj);response.success(function(data){if(!data.error){document.location.href="/thanks";clearFields(selector);}sendButton.removeClass('load');active=true;});response.error(function(data){console.log(data);sendButton.removeClass('load');active=true;});}});
+$(document).ready(function(){
+    // Предотвращение отправки формы через php при нажатии на кнопку
+    $('form').submit(function (e) {
+        e.stopPropagation();
+        e.preventDefault();
+    });
+
+    // Проверка почты на соответствие маске *@*.*
+    function isEmail( mail ){
+        var regex = /\S+@\S+/igm;
+        return regex.test(mail);
+    }
+
+
+    // Очистка формы
+    function clearFields( selector ){
+        $(selector).each(function(){
+            if( $(this).attr('name') != 'form'){
+                $(this).val('');
+                $(this).parent().removeClass('valid');
+            }
+        });
+    }
+
+
+    // Проверка отдельного поля на валидность
+    function fieldValid( input ) {
+
+        if( input.val() != '' ){
+
+            if ( input.attr('type') == 'email' ){
+                return isEmail( input.val() );
+            } else {
+                return true;
+            }
+
+        } else {
+            return false;
+        }
+
+    }
+
+
+    // Добавление классов полю по результатам валидации
+    function fieldCheck( input ) {
+
+        var parent = input.parent();
+
+        if( fieldValid( input ) ){
+            if( parent.hasClass('error') ){
+                parent.removeClass('error')
+            }
+            parent.addClass('valid');
+
+            return true;
+        } else {
+            if( parent.hasClass('valid') ){
+                parent.removeClass('valid')
+            }
+            parent.addClass('error');
+
+            return false;
+        }
+    }
+
+    $('.form-input').on('change', function () {
+        return fieldCheck( $(this) );
+    });
+    $('.form-input').on('focusout', function () {
+        $(this).parent().removeClass('filling');
+    });
+    $('.form-input').on('input', function () {
+        $(this).parent().addClass('filling');
+    });
+
+
+    // Проверка полей формы на отсутствие пустых полей и валидность
+    function fieldsCheck( selector ){
+        var checked = true;
+        var focus = true;
+        $(selector).each( function () {
+            checked = fieldCheck( $(this) ) && checked;
+            if ( focus && !checked ){
+                $(this).focus();
+                focus = false;
+            }
+        });
+        return checked;
+    }
+
+
+    // Сбор данных с формы и формирование объекта
+    function addFields( selector, object ){
+
+        $(selector).each(function () {
+            var $this = $(this);
+
+            object[$this.attr('name')] = $this.val();
+        });
+    }
+
+    function ajaxDataSend(type, url, data) {
+        return $.ajax(
+            {
+                type: type,
+                url: url,
+                dataType: 'json',
+                data: data,
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            }
+        );
+    }
+
+    /*window.onSubmitReCaptcha = function (token) {
+        active = false;
+        sendButton.addClass('load');
+
+        console.log('Captcha test start');
+        var cResponse = {'g-recaptcha-response': grecaptcha.getResponse()};
+
+        var capchaTest = ajaxDataSend('POST', '/captcha', cResponse);
+
+        capchaTest.success(function(data){
+            if( data.error ){
+                alert('Проверка не пройдена!');
+                sendButton.removeClass('load');
+                active = true;
+            }else{
+                console.log('CaptchaTest success');
+                sendForm();
+            }
+        });
+        capchaTest.error(function(data){
+            alert('При отправке сообщения произошла ошибка');
+            console.log('CaptchaTest error');
+            sendButton.removeClass('load');
+            active = true;
+        });
+    };*/
+
+
+    var unical, sendButton, selector;
+    var active = true;
+    // var thanks_show = false;
+
+    function finalValidation() {
+        if( active ){
+            sendButton = $(this);
+            unical = sendButton.closest('.form-id').attr('id');
+            selector = '#'+unical+' .form-input';
+
+            var validForm  = fieldsCheck( selector );
+
+            if ( validForm ){
+                /*grecaptcha.reset();
+                grecaptcha.execute();*/
+                active = false;
+                /*if (unical == 'feedback_call' || unical == 'flats_call'){
+                    thanks_show = true;
+                }*/
+                sendButton.addClass('load');
+                sendForm();
+            }
+        }
+    }
+
+    $('.send-form').on('click', finalValidation);
+
+
+    function sendForm() {
+
+        var dataobj = {};
+
+        addFields( selector, dataobj );
+
+        var response = ajaxDataSend('POST', '/feedback/mail', dataobj);
+        response.success(function(data){
+            if(!data.error){
+                /*if (thanks_show){
+                    $('.thank').click();
+                } else {
+                    $('.white-popup__thanks-message').fadeIn();
+                    setTimeout(function () {
+                        $('.white-popup__thanks-message').fadeOut(1000);
+                    }, 5000);
+                }*/
+                document.location.href="/thanks";
+                clearFields( selector );
+            }
+            // thanks_show = false;
+            sendButton.removeClass('load');
+            active = true;
+        });
+        response.error(function(data){
+            console.log(data);
+            sendButton.removeClass('load');
+            active = true;
+            // thanks_show = false;
+        });
+    }
+});
--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,7 +1,7 @@
 @section('footer')
     <footer class="footer">
         <div class="footer__wrap">
-            <p class="footer__copy">© 2017 Prestige</p>
+            <p class="footer__copy">© 2017 ЖК «Престиж»</p>
             <div class="footer__wonderbar-wrap">
                 <p class="footer__help">Помогает продавать</p>
                 <img src="/img/wonderbar.png" alt="Маркетинговое агенство Wonderbar" class="footer__img">
