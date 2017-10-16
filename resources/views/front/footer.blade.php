7d818571df758349f6a998195e91dcca88360cde
Kaluchii <mr.kaluchii@mail.ru>
2017-10-16 21:13:09 +0600

    Add disclaimer in footer

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -2,6 +2,7 @@
     <footer class="footer">
         <div class="footer__wrap">
             <p class="footer__copy">© 2017 ЖК «Престиж»</p>
+            <p class="footer__disclaimer">Информация предоставлена с целью изучения спроса потенциальных потребителей. Договоры о долевом участии заключаются после получения разрешения на привлечение денег дольщиков местным исполнительным органом согласно Закона РК «О долевом участии в жилищном строительстве».</p>
             <div class="footer__wonderbar-wrap">
                 <p class="footer__help">Помогает продавать</p>
                 <img src="/img/wonderbar.png" alt="Маркетинговое агенство Wonderbar" class="footer__img">
